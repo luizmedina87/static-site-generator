36e240601d39a396dbe186bb15efd6b873eb0423
Luiz Medina <luizmedina87@gmail.com>
2026-07-15 10:10:31 -0400

    add function and tests to convert a block of text to its block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     dirty_blocks = markdown.split("\n\n")
     clean_blocks = []
@@ -6,3 +18,39 @@ def markdown_to_blocks(markdown: str) -> list[str]:
             continue
         clean_blocks.append(block.strip())
     return clean_blocks
+
+
+def test_beginning_block(block: str, char: str) -> bool:
+    split = block.split('\n')
+    test = True
+    for line in split:
+        if line[0] != char:
+            test = False
+    return test
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
