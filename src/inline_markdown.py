59e363aa790a876c9d265e9ece6909ed8295f40a
Luiz Medina <luizmedina87@gmail.com>
2026-07-16 15:02:39 -0400

    reorder for clarity

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -3,6 +3,16 @@ import re
 from textnode import TextType, TextNode
 
 
+def text_to_textnodes(text: str) -> list[TextNode]:
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, '`', TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, '**', TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, '_', TextType.ITALIC)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
 def split_nodes_delimiter(
     old_nodes: list[TextNode],
     delimiter: str,
@@ -93,12 +103,3 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
             new_nodes.append(text_node)
     return new_nodes
 
-
-def text_to_textnodes(text: str) -> list[TextNode]:
-    nodes = [TextNode(text, TextType.TEXT)]
-    nodes = split_nodes_delimiter(nodes, '`', TextType.CODE)
-    nodes = split_nodes_delimiter(nodes, '**', TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, '_', TextType.ITALIC)
-    nodes = split_nodes_image(nodes)
-    nodes = split_nodes_link(nodes)
-    return nodes
