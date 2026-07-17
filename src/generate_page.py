cfb94984aa307a51e71313db81ee69abd55b02a3
Luiz Medina <luizmedina87@gmail.com>
2026-07-17 16:09:00 -0400

    add feature to generate html page from markdown file

---
+++ src/generate_page.py
@@ -0,0 +1,39 @@
+import os
+
+from markdown_blocks import markdown_to_html_node
+from inline_markdown import extract_title
+
+
+TITLE_PATTERN = "{{ Title }}"
+CONTENT_PATTERN = "{{ Content }}"
+
+
+def generate_page(
+        from_path: str, 
+        template_path: str, 
+        dest_path: str
+    ) -> None:
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as from_file:
+        from_content = from_file.read()
+    with open(template_path, "r") as template_file:
+        template_content = template_file.read()
+    if not from_content:
+        raise Exception(f"{from_path} is empty")
+    if not template_file:
+        raise Exception(f"{template_file} is empty")
+    
+    html_title = extract_title(from_content)
+    html_tree = markdown_to_html_node(from_content)
+    html_string = html_tree.to_html()
+    dest_content = (
+        template_content.replace(TITLE_PATTERN, html_title)
+                        .replace(CONTENT_PATTERN, html_string)
+    )
+    
+    dest_dir = os.path.dirname(dest_path)
+    os.makedirs(dest_dir, exist_ok="True")
+
+    with open(dest_path, "w") as dest_file:
+        dest_file.write(dest_content)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+import os
+
 from copystatic import replace_static_public
+from generate_page import generate_page
+from constants import (
+    INDEX_MD,
+    TEMPLATE_HTML,
+    INDEX_HTML,
+)
 
 
 def main():
     replace_static_public()
+    generate_page(INDEX_MD, TEMPLATE_HTML, INDEX_HTML)
 
 
 main()
